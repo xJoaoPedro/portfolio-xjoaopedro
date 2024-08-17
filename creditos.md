974faec9be056ed75302b0b7b41350efeeeeac57
JonesPedD <pedjoness28@gmail.com>
2024-08-17 19:56:58 -0300

    about me part

---
+++ creditos.md
@@ -0,0 +1,3 @@
+<a href="https://iconscout.com/icons/arduino" class="text-underline font-size-sm" target="_blank">arduino</a> by <a href="https://iconscout.com/pt/contributors/icon-mafia/:asset" class="text-underline font-size-sm">Icon Mafia</a> on <a href="https://iconscout.com" class="text-underline font-size-sm">IconScout</a>
+
+<a href="https://www.flaticon.com/br/icones-gratis/html-5" title="html 5 ícones">Html 5 ícones criados por Pixel perfect - Flaticon</a>
--- scripts/animationText.js
+++ scripts/animationText.js
@@ -1,11 +1,17 @@
 let textElement = document.getElementById('title');
+let textElement2 = document.getElementById('textAboutMe');
 const baseText = "xJoaoPedro";
+const baseText2 = "Hello World!";
+
 
 function alternarTexto() {
     if (textElement.textContent.endsWith('_')) {
         textElement.textContent = baseText;
+        textElement2.textContent = baseText2;
     } else {
         textElement.textContent += '_';
+        textElement2.textContent = baseText2.slice(0, -1);
+        textElement2.textContent += '_';
     }
 }
 
