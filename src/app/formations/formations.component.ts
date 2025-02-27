6156e26a081a0da1074bf18453b6923860d1bbd1
xJoaoPedro <pedjoness28@gmail.com>
2025-02-27 16:52:52 -0300

    formações reta

--- src/app/formations/formations.component.ts
+++ src/app/formations/formations.component.ts
@@ -42,19 +42,19 @@ export class FormationsComponent {
 
     switch (difference) {
       case 0: // Item selecionado
-        return 'z-10 left-1/2 transform -translate-x-1/2 scale-110 opacity-100';
+        return 'z-10 left-1/2 transform -translate-x-1/2 scale-100 opacity-100';
       case 1: // Próximo item
-        return 'z-[9] right-[28%] transform translate-x-1/2 translate-y-[40px] scale-75 opacity-80';
+        return 'z-[9] right-[28%] transform translate-x-1/2 translate-y-[50px] scale-[70%] opacity-80';
       case 2: // Segundo à direita
-        return 'z-[8] right-[8%] transform -translate-x-[30%] translate-y-[100px] scale-[80%] opacity-50';
+        return 'z-[8] right-[8%] transform translate-x-[30%] translate-y-[110px] scale-[40%] opacity-50';
       case this.items.length - 1: // Item anterior (à esquerda)
-        return 'z-5 left-1/4 transform -translate-x-1/2 translate-y-[30px] scale-[60%] opacity-70';
+        return 'z-[9] left-[28%] transform -translate-x-1/2 translate-y-[50px] scale-[70%] opacity-80';
       case this.items.length - 2: // Segundo à esquerda
-        return 'z-4 left-[10%] transform -translate-x-1/2 translate-y-[20px] scale-[40%] opacity-50';
+        return 'z-[8] left-[8%] transform -translate-x-[30%] translate-y-[110px] scale-[40%] opacity-50';
       case 3: // escondido a direita
-        return 'opacity-0 transform scale-[0.2] left-[110%] top-1/2';
+        return 'right-[-32%] transform -translate-x-1/2 ease-in-out z-6 scale-[25%] opacity-10';
       case 4:
-        return 'opacity-0 transform scale-[0.2] left-[-110%] top-1/2';
+        return 'left-[-32%] transform -translate-x-1/2 ease-in-out z-6 scale-[25%] opacity-10';
       default:
         return 'transform scale-[0.2] left-1/2';
 
